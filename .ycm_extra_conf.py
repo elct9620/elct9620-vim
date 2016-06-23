26c58b7d0d373508594ff527cd5ceac687412b5a
蒼時弦也 <elct9620@frost.tw>
2016-06-23 21:14:05 +0800

    Optimize

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -61,12 +61,29 @@ flags = [
 'c++',
 # This path will only work on OS X, but extra paths that don't exist are not
 # harmful
+'/System/Library/Frameworks/Python.framework/Headers',
+'-isystem',
+'../llvm/include',
+'-isystem',
+'../llvm/tools/clang/include',
 '-I',
 '.',
-'-isystem',
-'/usr/include',
 '-I',
-'/usr/local/include',
+'./include',
+'-I',
+'./src',
+'-I',
+'./ClangCompleter',
+'-isystem',
+'./tests/gmock/gtest',
+'-isystem',
+'./tests/gmock/gtest/include',
+'-isystem',
+'./tests/gmock',
+'-isystem',
+'./tests/gmock/include',
+'-I',
+'/usr/local/include'
 ]
 
 
---
+++ plugin/settings/Syntastic.vim
@@ -0,0 +1,4 @@
+let g:syntastic_typescript_tsc_args = '--jsx'
+
+let g:syntastic_cpp_compiler = 'clang++'
+let g:syntastic_cpp_compiler_options = ' -std=c++11 -stdlib=libc++'
